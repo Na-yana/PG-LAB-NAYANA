1024ea32cd2e8de9ac5fab4e80862e9aab04b6be
Na-yana <93870204+Na-yana@users.noreply.github.com>
2021-12-20 13:05:13 +0530

    Add files via upload

---
+++ m.py
@@ -0,0 +1,19 @@
+help('modules')
+
+import math
+print(math.pi)
+
+import math as m
+print(m.pi)
+
+from math import pi,sqrt
+print (math.pi)
+print(sqrt(5))
+
+print (math.cos(90))
+print (math.sin(30))
+print(math.tan(45))
+
+
+
+
